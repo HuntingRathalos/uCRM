8ad96af80220d535c408ec73dfaf7fd784ce6c52
HuntingRathalos <huntingandrathalos@gmail.com>
2022-09-28 12:01:43 +0900

    laravel breezeインストール #1

---
+++ resources/js/Pages/Inertia/index.vue
@@ -0,0 +1,4 @@
+<script setup></script>
+<template>
+    <p>aaaa</p>
+</template>
---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { Link } from "@inertiajs/inertia-vue3";
+</script>
+<template>
+    testです。
+    <a href="/">aaa</a>
+    <Link href="/">link</Link><br />
+    <Link :href="route('inertia.index')">名前付きルート</Link>
+</template>
