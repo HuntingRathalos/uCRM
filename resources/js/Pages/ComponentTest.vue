bead4d445025c574d6f680d9cf2547121411fd66
HuntingRathalos <huntingandrathalos@gmail.com>
2022-09-30 13:55:05 +0900

    defineEmitの挙動確認 #1

--- resources/js/Pages/ComponentTest.vue
+++ resources/js/Pages/ComponentTest.vue
@@ -2,10 +2,15 @@
 import GuestLayoutVue from "@/Layouts/GuestLayout.vue";
 import InputLabelVue from "@/Components/InputLabel.vue";
 import TextInputVue from "@/Components/TextInput.vue";
+
+const emitTest = (e) => console.log(e);
 </script>
 <template>
     <GuestLayoutVue>
         <InputLabelVue value="件名">タイトル</InputLabelVue>
-        <TextInputVue modelValue="初期値初期値"></TextInputVue>
+        <TextInputVue
+            @update:modelValue="emitTest"
+            modelValue="初期値初期"
+        ></TextInputVue>
     </GuestLayoutVue>
 </template>
--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,14 @@ import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-    server: {
-        hmr: {
-            host: "localhost",
-        },
-    },
+    // server: {
+    //     hmr: {
+    //         host: localhost,
+    //     },
+    // },
+    // server: {
+    //     host: true,
+    // },
     plugins: [
         laravel({
             input: "resources/js/app.js",
