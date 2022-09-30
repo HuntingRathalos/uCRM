ba55707f8fa34fe20b439e2f7d77a21499a6dd96
HuntingRathalos <huntingandrathalos@gmail.com>
2022-09-30 13:11:21 +0900

    別コンポーネントのインポート、挙動確認 #1

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,11 @@
+<script setup>
+import GuestLayoutVue from "@/Layouts/GuestLayout.vue";
+import InputLabelVue from "@/Components/InputLabel.vue";
+import TextInputVue from "@/Components/TextInput.vue";
+</script>
+<template>
+    <GuestLayoutVue>
+        <InputLabelVue value="件名">タイトル</InputLabelVue>
+        <TextInputVue modelValue="初期値初期値"></TextInputVue>
+    </GuestLayoutVue>
+</template>
