5220aca23ac55a18c096f464246966f989d4b1ca
HuntingRathalos <huntingandrathalos@gmail.com>
2022-10-02 02:51:35 +0900

    FlashMessageコンポーネント作成 #3

--- resources/js/Pages/Items/Index.vue
+++ resources/js/Pages/Items/Index.vue
@@ -1,6 +1,7 @@
 <script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { Head, Link } from "@inertiajs/inertia-vue3";
+import FlashMessage from "@/Components/FlashMessage.vue";
 defineProps({
     items: Array,
 });
@@ -21,6 +22,7 @@ defineProps({
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
                         <section class="text-gray-600 body-font">
+                            <FlashMessage />
                             <div class="container px-5 py-8 mx-auto">
                                 <div
                                     class="flex pl-4 my-4 lg:w-2/3 w-full mx-auto"
