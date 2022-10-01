5314c38b50527af494989b2b8cb531cc45a919c2
HuntingRathalos <huntingandrathalos@gmail.com>
2022-10-02 02:31:13 +0900

    バリデーション実装（Store) #3

--- resources/js/Components/InputError.vue
+++ resources/js/Components/InputError.vue
@@ -1,11 +1,20 @@
 <script setup>
-defineProps(['message']);
+import { usePage } from "@inertiajs/inertia-vue3";
+import { computed } from "@vue/reactivity";
+
+// defineProps(["message"]);
+const message = computed(() => usePage().props.value.errors);
+const hasErrors = computed(() => Object.keys(message.value).length > 0);
 </script>
 
 <template>
-    <div v-show="message">
-        <p class="text-sm text-red-600">
+    <div v-show="hasErrors">
+        <div class="font-medium text-red-600">問題が発生しました。</div>
+        <ul class="mt-3 list-disc list-inside text-sm text-red-600">
+            <li v-for="(error, key) in message" :key="key">{{ error }}</li>
+        </ul>
+        <!-- <p class="text-sm text-red-600">
             {{ message }}
-        </p>
+        </p> -->
     </div>
 </template>
--- resources/js/Pages/Items/Create.vue
+++ resources/js/Pages/Items/Create.vue
@@ -3,6 +3,7 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { Head } from "@inertiajs/inertia-vue3";
 import { reactive } from "vue";
 import { Inertia } from "@inertiajs/inertia";
+import InputError from "@/Components/InputError.vue";
 
 defineProps({
     errors: Object,
@@ -33,6 +34,7 @@ const storeItem = () => {
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
+                        <InputError class="mt-2" :message="errors" />
                         <section class="text-gray-600 body-font relative">
                             <form @submit.prevent="storeItem">
                                 <div class="container px-5 py-8 mx-auto">
