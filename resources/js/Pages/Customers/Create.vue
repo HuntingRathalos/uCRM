04e5b1a6593e3b951141f7522bdc786af2015b2d
HuntingRathalos <huntingandrathalos@gmail.com>
2022-10-07 23:27:08 +0900

    Customers/Create修正（storeCustomer実装） #5

--- resources/js/Pages/Customers/Create.vue
+++ resources/js/Pages/Customers/Create.vue
@@ -21,8 +21,8 @@ const form = reactive({
     memo: null,
 });
 
-const storeItem = () => {
-    Inertia.post("/items", form);
+const storeCustomer = () => {
+    Inertia.post("/customers", form);
 };
 </script>
 
@@ -42,7 +42,7 @@ const storeItem = () => {
                     <div class="p-6 bg-white border-b border-gray-200">
                         <InputError class="mt-2" :message="errors" />
                         <section class="text-gray-600 body-font relative">
-                            <form @submit.prevent="storeItem">
+                            <form @submit.prevent="storeCustomer">
                                 <div class="container px-5 py-8 mx-auto">
                                     <div class="lg:w-1/2 md:w-2/3 mx-auto">
                                         <div class="flex flex-wrap -m-2">
