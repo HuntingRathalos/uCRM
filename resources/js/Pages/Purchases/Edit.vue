4a04d58601168920a87c77426e7982ca7bc4c2ca
HuntingRathalos <huntingandrathalos@gmail.com>
2022-10-29 04:53:01 +0900

    PurchaseControllerのUpdateメソッド追加 #9

--- resources/js/Pages/Purchases/Edit.vue
+++ resources/js/Pages/Purchases/Edit.vue
@@ -45,7 +45,7 @@ const updatePurchase = (id) => {
             form.items.push({ id: item.id, quantity: item.quantity });
         }
     });
-    Inertia.put(route("purchases.update"), { purchase: id }, form);
+    Inertia.put(route("purchases.update", { purchase: id }), form);
 };
 
 const quantity = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
