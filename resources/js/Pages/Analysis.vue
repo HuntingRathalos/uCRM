304878ef7591abfe514fde000c5628a7151cc88c
HuntingRathalos <huntingandrathalos@gmail.com>
2022-11-04 10:42:42 +0900

    Api通信の接続確認 #11

--- resources/js/Pages/Analysis.vue
+++ resources/js/Pages/Analysis.vue
@@ -11,7 +11,25 @@ onMounted(() => {
 const form = reactive({
     startDate: null,
     endDate: null,
+    type: "perDay",
 });
+const getData = async () => {
+    try {
+        await axios
+            .get("/api/analysis/", {
+                params: {
+                    startDate: form.startDate,
+                    endDate: form.endDate,
+                    type: form.type,
+                },
+            })
+            .then((res) => {
+                console.log(res.data);
+            });
+    } catch (e) {
+        console.log(e.message);
+    }
+};
 </script>
 
 <template>
@@ -28,11 +46,19 @@ const form = reactive({
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
-                        <form>
-                            From: <input type=date/ name="startDate"
-                            v-model="form.startDate"><br />
-                            To: <input type=date/ name="endDate"
-                            v-model="form.endDate"/>
+                        <form @submit.prevent="getData">
+                            From:
+                            <input
+                                type="date"
+                                name="startDate"
+                                v-model="form.startDate"
+                            /><br />
+                            To:
+                            <input
+                                type="date"
+                                name="endDate"
+                                v-model="form.endDate"
+                            />
                             <button
                                 class="mt-4 flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
                             >
