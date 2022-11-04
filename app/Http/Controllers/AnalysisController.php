82769438ef8aca38b6e784412eff09707b426d53
HuntingRathalos <huntingandrathalos@gmail.com>
2022-11-04 09:08:55 +0900

    Analysis.vue作成（フォーム追加） #11

--- app/Http/Controllers/AnalysisController.php
+++ app/Http/Controllers/AnalysisController.php
@@ -11,14 +11,16 @@ class AnalysisController extends Controller
 {
     public function index()
     {
-        $period = Order::betweenDate($startDate, $endDate)-
-        >groupBy('id')
+        $startDate = '2022-08-01';
+        $endDate = '2022-08-31';
+
+        $period = Order::betweenDate($startDate, $endDate)->groupBy('id')
         ->selectRaw('id, sum(subtotal) as total,
         customer_name, status, created_at')
         ->orderBy('created_at')
         ->paginate(50);
 
-        dd($period);
+        // dd($period);
 
         return Inertia::render('Analysis');
     }
