8762d7db6a28b12a88fe3b169852542b01f7e267
HuntingRathalos <huntingandrathalos@gmail.com>
2022-11-07 06:02:06 +0900

    AnalysisService作成 #13

--- app/Http/Controllers/Api/AnalysisController.php
+++ app/Http/Controllers/Api/AnalysisController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Models\Order;
 use Illuminate\Support\Facades\DB;
+use App\Services\AnalysisService;
 
 class AnalysisController extends Controller
 {
@@ -15,19 +16,9 @@ class AnalysisController extends Controller
         $subQuery = Order::betweenDate($request->startDate, $request->endDate);
         if($request->type === 'perDay')
         {
-        $subQuery->where('status', true)->groupBy('id')
-        ->selectRaw('SUM(subtotal) AS totalPerPurchase, DATE_FORMAT(created_at, "%Y%m%d") AS date')
-        ->groupBy('date');
-
-        $data = DB::table($subQuery)
-        ->groupBy('date')
-        ->selectRaw('date, sum(totalPerPurchase) as total')
-        ->get();
+            list($data, $labels, $totals) = AnalysisService::perDay($subQuery);
         }
 
-        // Chart.js用にデータを配列に加工
-        $labels = $data->pluck('date');
-        $totals = $data->pluck('total');
 
         return response()->json([
             'data' => $data,
