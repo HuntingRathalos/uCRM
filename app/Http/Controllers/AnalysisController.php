7c1853af3064d30d268fc398f1ac14d3c8899edb
HuntingRathalos <huntingandrathalos@gmail.com>
2022-11-12 16:35:26 +0900

    クエリ作成（連番を振る） #13

--- app/Http/Controllers/AnalysisController.php
+++ app/Http/Controllers/AnalysisController.php
@@ -46,6 +46,17 @@ class AnalysisController extends Controller
         as total')
         ->orderBy('total', 'desc');
 
+        // 一部のデータベース操作文は値を返しません。こうしたタイプの操作では、DBファサードでstatementメソッドを使用します。
+        // set @変数名 = 値 (mysqlの書き方)
+        // 3. 購入順に連番を振る
+        DB::statement('set @row_num = 0;');
+        $subQuery = DB::table($subQuery)
+        ->selectRaw('
+        @row_num:= @row_num+1 as row_num,
+        customer_id,
+        customer_name,
+        total');
+
         return Inertia::render('Analysis');
     }
 }
