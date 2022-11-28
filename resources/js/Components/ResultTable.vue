acf69e824389d9ace6aa7d6b52cb86fa9e2650fe
HuntingRathalos <huntingandrathalos@gmail.com>
2022-11-28 20:17:10 +0900

    スタイル調整 #17

--- resources/js/Components/ResultTable.vue
+++ resources/js/Components/ResultTable.vue
@@ -71,16 +71,16 @@ const props = defineProps({
             </thead>
             <tbody>
                 <tr v-for="item in data.data">
-                    <td class="px-4 py-3">
+                    <td class="border-b-2 border-green-200 px-4 py-3">
                         {{ item.decile }}
                     </td>
-                    <td class="px-4 py-3">
+                    <td class="border-b-2 border-green-200 px-4 py-3">
                         {{ item.average }}
                     </td>
-                    <td class="px-4 py-3">
+                    <td class="border-b-2 border-green-200 px-4 py-3">
                         {{ item.totalPerGroup }}
                     </td>
-                    <td class="px-4 py-3">
+                    <td class="border-b-2 border-green-200 px-4 py-3">
                         {{ item.totalRatio }}
                     </td>
                 </tr>
@@ -91,44 +91,109 @@ const props = defineProps({
         v-if="data.type === 'rfm'"
         class="lg:w-2/3 w-full mx-auto overflow-auto"
     >
-        {{ data.totals }}
-        <table class="mx-auto">
+        <h2 class="text-center text-2xl my-4">RFM 分析結果</h2>
+        <div class="my-4 text-center">
+            {{ data.totals }}
+        </div>
+        <h3 class="text-center text-2xl my-4">RFMランク毎の人数</h3>
+        <table class="table-auto w-full text-left whitespace-no-wrap">
             <thead>
                 <tr>
-                    <th>ランク</th>
-                    <th>R (○日以内)</th>
-                    <th>F (○回以上)</th>
-                    <th>M (○円以上)</th>
+                    <th
+                        class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl"
+                    >
+                        ランク
+                    </th>
+                    <th
+                        class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl"
+                    >
+                        R (○日以内)
+                    </th>
+                    <th
+                        class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl"
+                    >
+                        F (○回以上)
+                    </th>
+                    <th
+                        class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl"
+                    >
+                        M (○円以上)
+                    </th>
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="rfm in data.eachCount" :key="rfm.rank">
-                    <td>{{ rfm.rank }}</td>
-                    <td>{{ rfm.r }}</td>
-                    <td>{{ rfm.f }}</td>
-                    <td>{{ rfm.m }}</td>
+                    <td class="border-b-2 border-green-200 px-4 py-3">
+                        {{ rfm.rank }}
+                    </td>
+                    <td class="border-b-2 border-green-200 px-4 py-3">
+                        {{ rfm.r }}
+                    </td>
+                    <td class="border-b-2 border-green-200 px-4 py-3">
+                        {{ rfm.f }}
+                    </td>
+                    <td class="border-b-2 border-green-200 px-4 py-3">
+                        {{ rfm.m }}
+                    </td>
                 </tr>
             </tbody>
         </table>
-        <table class="mx-auto">
+        <h3 class="text-center text-2xl my-4">RとFの集計表</h3>
+        <table class="table-auto w-full text-left whitespace-no-wrap">
             <thead>
                 <tr>
-                    <th>rRank</th>
-                    <th>f_5</th>
-                    <th>f_4</th>
-                    <th>f_3</th>
-                    <th>f_2</th>
-                    <th>f_1</th>
+                    <th
+                        class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl"
+                    >
+                        rRank
+                    </th>
+                    <th
+                        class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl"
+                    >
+                        f_5
+                    </th>
+                    <th
+                        class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl"
+                    >
+                        f_4
+                    </th>
+                    <th
+                        class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl"
+                    >
+                        f_3
+                    </th>
+                    <th
+                        class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl"
+                    >
+                        f_2
+                    </th>
+                    <th
+                        class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl"
+                    >
+                        f_1
+                    </th>
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="rf in data.data" :key="rf.rRank">
-                    <td>{{ rf.rRank }}</td>
-                    <td>{{ rf.f_5 }}</td>
-                    <td>{{ rf.f_4 }}</td>
-                    <td>{{ rf.f_3 }}</td>
-                    <td>{{ rf.f_2 }}</td>
-                    <td>{{ rf.f_1 }}</td>
+                    <td class="border-b-2 border-green-200 px-4 py-3">
+                        {{ rf.rRank }}
+                    </td>
+                    <td class="border-b-2 border-green-200 px-4 py-3">
+                        {{ rf.f_5 }}
+                    </td>
+                    <td class="border-b-2 border-green-200 px-4 py-3">
+                        {{ rf.f_4 }}
+                    </td>
+                    <td class="border-b-2 border-green-200 px-4 py-3">
+                        {{ rf.f_3 }}
+                    </td>
+                    <td class="border-b-2 border-green-200 px-4 py-3">
+                        {{ rf.f_2 }}
+                    </td>
+                    <td class="border-b-2 border-green-200 px-4 py-3">
+                        {{ rf.f_1 }}
+                    </td>
                 </tr>
             </tbody>
         </table>
