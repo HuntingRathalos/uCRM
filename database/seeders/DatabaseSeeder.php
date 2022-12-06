504bb88e961ad6a63c33daa82dd4942b9ad1be7a
HuntingRathalos <huntingandrathalos@gmail.com>
2022-12-06 22:09:16 +0900

    RankTable、RankSeeder作成 #19

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,6 +26,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             ItemSeeder::class,
+            RankSeeder::class,
         ]);
 
         Customer::factory(1000)->create();
